053d214bd000f82bc953c719b7e8daa1d582917f
alessandrovaru <alessandrovaru@gmail.com>
2022-09-29 09:08:37 -0400

    push and merge

--- src/pages/index.md
+++ src/pages/index.md
@@ -1,39 +1,9 @@
 ---
-fourthImage:
-  linkYoutube: https://www.youtube.com/watch?v=TY0FMPZBWHM&ab_channel=Yarge
-  linkSpotify: https://open.spotify.com/track/5l9D9Uzeba8H3vVbEEoWnd
-  subtitle: Yarge ft. Suei
-  title: Nublado
-  image: /img/flavor_wheel.jpg
-heading: Great coffee with a conscience
-subheading: Productora audiovisual | Conceptualización | Postproducción
 templateKey: index-page
 title: Único Nortew
-subtitle: Great coffee with a conscience
-firstImage:
-  title: Nublado
-  subtitle: Yarge ft. Sueis
-  linkYoutube: https://www.youtube.com/watch?v=TY0FMPZBWHM&ab_channel=Yarge
-  linkSpotify: https://open.spotify.com/track/5l9D9Uzeba8H3vVbEEoWnd
-  image: /img/blog-index.jpg
-SecondImage:
-  title: Nublado
-  subtitle: Yarge ft. Suei
-  linkYoutube: https://www.youtube.com/watch?v=TY0FMPZBWHM&ab_channel=Yarge
-  linkSpotify: https://open.spotify.com/track/5l9D9Uzeba8H3vVbEEoWnd
-  image: /img/blog-index.jpg
-fifthImage:
-  linkYoutube: https://www.youtube.com/watch?v=TY0FMPZBWHM&ab_channel=Yarge
-  linkSpotify: https://open.spotify.com/track/5l9D9Uzeba8H3vVbEEoWnd
-  subtitle: Yarge ft. Suei
-  title: Nublado
-  image: /img/home-jumbotron.jpg
-sixthImage:
-  linkYoutube: https://www.youtube.com/watch?v=TY0FMPZBWHM&ab_channel=Yarge
-  linkSpotify: https://open.spotify.com/track/5l9D9Uzeba8H3vVbEEoWnd
-  subtitle: Yarge ft. Suei
-  title: Nublado
-  image: /img/products-full-width.jpg
+image: /img/la-vida-boheme-carrusel-suzie-kamikaze-24.jpg
+heading: Great coffee with a conscience
+subheading: Productora audiovisual | Conceptualización | Postproducción
 mainpitch:
   title: Sigue tu norte, sé único
   description: >
@@ -42,6 +12,10 @@ mainpitch:
     Morbi eget justo neque. Fusce feugiat dapibus odio. Integer id nulla quis
     dui lobortis posuere. Vivamus urna elit, consectetur sed lacus non, dictum
     consectetur nisi.
+description: Kaldi is the ultimate spot for coffee lovers who want to learn
+  about their java’s origin and support the farmers that grew it. We take coffee
+  production, roasting and brewing seriously and we’re glad to pass that
+  knowledge to anyone.
 intro:
   blurbs:
     - image: /img/playboi-carti-sky-sky-music-video.gif
@@ -52,12 +26,6 @@ intro:
   description: >
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in rhoncus
     est, sit amet consequat augue. 
-thirdImage:
-  linkYoutube: https://www.youtube.com/watch?v=TY0FMPZBWHM&ab_channel=Yarge
-  linkSpotify: https://open.spotify.com/track/5l9D9Uzeba8H3vVbEEoWnd
-  subtitle: Yarge ft. Suei
-  title: Nublado
-  image: /img/blog-index.jpg
 main:
   heading: Great coffee with no compromises
   description: >
@@ -74,9 +42,4 @@ main:
   image3:
     alt: Coffee beans
     image: /img/products-grid1.jpg
-image: /img/la-vida-boheme-carrusel-suzie-kamikaze-24.jpg
-description: Kaldi is the ultimate spot for coffee lovers who want to learn
-  about their java’s origin and support the farmers that grew it. We take coffee
-  production, roasting and brewing seriously and we’re glad to pass that
-  knowledge to anyone.
 ---
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,31 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { graphql } from "gatsby";
-// import { getImage } from "gatsby-plugin-image";
+import { graphql, Link } from "gatsby";
+import { getImage } from "gatsby-plugin-image";
 
 import Layout from "../components/Layout";
-// import Features from "../components/Features";
-// import BlogRoll from "../components/BlogRoll";
+import Features from "../components/Features";
+import BlogRoll from "../components/BlogRoll";
 import MainCarrousel from "../components/main-carrousel/MainCarrousel"
-// import FullWidthImage from "../components/FullWidthImage";
+import FullWidthImage from "../components/FullWidthImage";
 
 // eslint-disable-next-line
 export const IndexPageTemplate = ({
+  image,
   title,
-  subtitle,
-  firstImage,
-  secondImage,
-  thirdImage,
-  fourthImage,
-  fifthImage,
-  sixthImage,
+  heading,
+  subheading,
+  mainpitch,
+  description,
+  intro,
 }) => {
-  // const heroImage = getImage(image) || image;
+  const heroImage = getImage(image) || image;
 
   return (
     <div className="index-carrousel">
-      {/* <FullWidthImage img={heroImage} title={title} subheading={subheading} /> */}
-      {/* <section className="section section--gradient">
+      <FullWidthImage img={heroImage} title={title} subheading={subheading} />
+      <section className="section section--gradient">
         <div className="container">
           <div className="section">
             <div className="columns">
@@ -47,7 +46,7 @@ export const IndexPageTemplate = ({
                       <p>{description}</p>
                     </div>
                   </div>
-                  <MainCarrousel/>
+                  {/* <MainCarrousel/> */}
                   <Features gridItems={intro.blurbs} />
                   <div className="columns">
                     <div className="column is-12 has-text-centered">
@@ -72,30 +71,22 @@ export const IndexPageTemplate = ({
             </div>
           </div>
         </div>
-      </section> */}
-      <MainCarrousel 
-        firstImage={firstImage}
-        secondImage={secondImage}
-        thirdImage={thirdImage}
-        fourthImage={fourthImage}
-        fifthImage={fifthImage}
-        sixthImage={sixthImage}
-        title={title}
-        subtitle={subtitle}
-      />
+      </section>
+      {/* <MainCarrousel/> */}
     </div>
   );
 };
 
 IndexPageTemplate.propTypes = {
+  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   title: PropTypes.string,
-  subtitle: PropTypes.string,
-  firstImage: PropTypes.object,
-  secondImage: PropTypes.object,
-  thirdImage: PropTypes.object,
-  fourthImage: PropTypes.object,
-  fifthImage: PropTypes.object,
-  sixthImage: PropTypes.object,
+  heading: PropTypes.string,
+  subheading: PropTypes.string,
+  mainpitch: PropTypes.object,
+  description: PropTypes.string,
+  intro: PropTypes.shape({
+    blurbs: PropTypes.array,
+  }),
 };
 
 const IndexPage = ({ data }) => {
@@ -104,14 +95,13 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <IndexPageTemplate
+        image={frontmatter.image}
         title={frontmatter.title}
-        subtitle={frontmatter.subtitle}
-        firstImage={frontmatter.firstImage}
-        secondImage={frontmatter.secondImage}
-        thirdImage={frontmatter.thirdImage}
-        fourthImage={frontmatter.fourthImage}
-        fifthImage={frontmatter.fifthImage}
-        sixthImage={frontmatter.sixthImage}
+        heading={frontmatter.heading}
+        subheading={frontmatter.subheading}
+        mainpitch={frontmatter.mainpitch}
+        description={frontmatter.description}
+        intro={frontmatter.intro}
       />
     </Layout>
   );
@@ -127,81 +117,39 @@ IndexPage.propTypes = {
 
 export default IndexPage;
 
-
-export const query = graphql`
-  
-    query IndexPageTemplate {
-      markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
-        frontmatter {
+export const pageQuery = graphql`
+  query IndexPageTemplate {
+    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+      frontmatter {
+        title
+        image {
+          childImageSharp {
+            gatsbyImageData(quality: 100, layout: FULL_WIDTH)
+          }
+        } 
+        heading
+        subheading
+        mainpitch {
           title
-          subtitle
-          firstImage {
-            title
-            subtitle
+          description
+        }
+        description
+        intro {
+          blurbs {
             image {
               childImageSharp {
-                gatsbyImageData(quality: 100, layout: FULL_WIDTH)
-              }
+                fluid(maxWidth: 500, quality: 92) {
+                  ...GatsbyImageSharpFluid
+                }
+              } 
+              publicURL
             }
-            linkYoutube
-            linkSpotify
+            text
           }
-          fourthImage {
-            title
-            subtitle
-            image {
-              childImageSharp {
-                gatsbyImageData(quality: 100, layout: FULL_WIDTH)
-              }
-            }
-            linkYoutube
-            linkSpotify
-          }
-          thirdImage {
-            title
-            subtitle
-            image {
-              childImageSharp {
-                gatsbyImageData(quality: 100, layout: FULL_WIDTH)
-              }
-            }
-            linkYoutube
-            linkSpotify
-          }
-          sixthImage {
-            linkYoutube
-            subtitle
-            image {
-              childImageSharp {
-                gatsbyImageData(quality: 100, layout: FULL_WIDTH)
-              }
-            }
-            title
-            linkSpotify
-          }
-          fifthImage {
-            subtitle
-            linkYoutube
-            image {
-              childImageSharp {
-                gatsbyImageData(quality: 100, layout: FULL_WIDTH)
-              }
-            }
-            title
-            linkSpotify
-          }
-          secondImage {
-            subtitle
-            linkYoutube
-            image {
-              childImageSharp {
-                gatsbyImageData(quality: 100, layout: FULL_WIDTH)
-              }
-            }
-            title
-            linkSpotify
-          }
+          heading
+          description
         }
       }
     }
+  }
 `;
